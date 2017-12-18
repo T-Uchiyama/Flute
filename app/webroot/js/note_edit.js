efb4e68dc542b4cee131900500413f07b55531fc
T-Uchiyama <t_uchiyama@funteam.co.jp>
2017-12-18 15:15:17 +0900

    コメント追加

--- app/webroot/js/note_edit.js
+++ app/webroot/js/note_edit.js
@@ -42,6 +42,7 @@ $(function(){
     var file_prefix = '/read/test.bmp';
     
     var htm = '';
+    // 指定範囲
     for (y = 0 ; y < tile_y ; y++){
       for (x = 0 ; x < tile_x ; x++){
         var w = tile_w;
@@ -70,6 +71,7 @@ $(function(){
     $('#draw_window').html(htm);
 
     htm = '';
+    // Preload範囲
     for (p = 0 ; p < pre_load_size ; p++){
       for (x = 0 ; x < tile_x ; x++){
         var pre_y = t_ost_y -1 - p;
@@ -98,19 +100,34 @@ $(function(){
       combine_fpath_id(o.prefix, file_prefix, o.y , o.x);
     });
   };
+  /**
+   * Image用のhtmlタグを生成
+   * @param  id_prefix    id定義
+   * @param  tile_y       指定されているy座標
+   * @param  tile_x       指定されているx座標
+   * @param  {String} [preload_id_prefix=''] Preload範囲用id定義 default = ''
+   * @param  {Number} [width=0]              画像のサイズ:横 default = 0
+   * @param  {Number} [height=0]             画像のサイズ:縦 default = 0
+   * @param  {Number} [pos_y=0]              y座標の表示開始位置 default = 0
+   * @param  {Number} [pos_x=0]              x座標の表示開始位置 default = 0
+   */
   function gen_img_tag(id_prefix, tile_y, tile_x, preload_id_prefix = '', width = 0, height = 0, pos_y = 0, pos_x = 0){
+    // x座標またはy座標が0以下で画像サイズが縦横0の場合は範囲外のため関数終了。
     if ((tile_x < 0 || tile_y < 0) && width == 0 && height == 0 ){
        return '';
     }
     var htm = '';
     var id = id_prefix + '_' + tile_y + '_' + tile_x;
     var src = '';
+    // 既に同様のIDが存在しており、srcが記載されている場合には記載されているsrcを取得。
     if ($('#' + id).length && $('#' + id).attr('src') != undefined && $('#' + id).attr('src').length){
       src = $('#' + id).attr('src');
     }
+    // 通常範囲
     if (width != 0 && height != 0){
       var preload_id = preload_id_prefix + '_' + tile_y + '_' + tile_x;
       var img_style = 'pointer-events: none;';
+      // pos_xとpos_yの位置調整
       if (pos_x < 0){
         pos_x = 0;
         img_style += 'right:0;'
@@ -119,35 +136,52 @@ $(function(){
         pos_y = 0;
         img_style += 'bottom:0;'
       }
+      // img_styleにposition:absoluteを追加
       if (img_style.length){
         img_style = 'style="position:absolute;' + img_style +'"';
-      } 
+      }
+      // srcが記載されておらず、Preload用のIDが存在し、既にsrc生成が終了している場合はPreloadIDを使用しsrcを取得
       if (src.length == 0 && $('#' + preload_id).length && $('#' + preload_id).attr('src') != undefined && $('#' + preload_id).attr('src').length){
         src = $('#' + preload_id).attr('src');
       }
+      // 以前のsrcがある場合にはそのまま流用
       if (src.length){
         src = ' src="' + src + '"';
       }
+      // 通常範囲のタグ生成
       htm += '<div style="width:' + width + 'px;height:' + height + 'px;position:absolute;left:'+ pos_x +'px;top:'+ pos_y +'px;overflow:hidden;">';
       htm += '<img id="' + id + '" width="'+ tile_w + '"height="'+ tile_h +'"' + img_style + src +' >\n';
       htm += '</div>';
     } else {
+        //Preload範囲の場合にはsrcをダイレクトで作成。
       if (src.length){
         src = ' src="' + src + '"';
       }
+      // Preload範囲のタグ生成
       htm += '<img id="' + id + '"' + src +' >\n';
     }
     return htm;
   }
+  /**
+   * 配列の要素よりファイルパスを結合・生成し、画像読み込みを実施。
+   * @param  id_prefix    id定義
+   * @param  fpath_prefix ファイルパス定義
+   * @param  tile_y       指定されているy座標
+   * @param  tile_x       指定されているx座標
+   */
   function combine_fpath_id(id_prefix, fpath_prefix, tile_y, tile_x){
+    // x座標またはy座標が0以下の場合には描画不必要な範囲のため関数を終了させる。
     if (tile_x < 0 || tile_y < 0){
        return;
     }
+    // idとurlの生成。
     var id = id_prefix + '_' + tile_y + '_' + tile_x;
     var url = fpath_prefix + '_y' + tile_y + '_x' + tile_x + '.png';
+    // 既に同様のIDが存在しており、読み込みが成功している場合には読み込み不必要であるため関数終了させる。
     if (($('#' + id).attr('src')) != undefined && ($('#' + id).attr('src')).length){
       return;
     }
+    // 画像読み込みを実施。
     load_img(url, id);
   }
   $('#draw_window').mousedown(function (e){
